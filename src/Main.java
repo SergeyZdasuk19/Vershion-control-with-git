dc32dfbac5a038f3afc1215daeb828be822bc970
Sergey Zdasuk <Sergey.zdasuk@tut.by>
2020-12-20 01:38:31 +0300

    method testTwo was added

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,11 @@ public class Main {
         System.out.println("Hello");
     }
 
-    public void testOne(){
+    public void testOne() {
         System.out.println("Test1");
+    }
+
+    public void testTwo() {
+        System.out.println("Test2");
     }
 }
