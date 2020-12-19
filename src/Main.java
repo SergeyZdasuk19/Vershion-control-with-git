a3f9c0954427bbf0bc3650d0f5cfd90db8834af7
Sergey Zdasuk <Sergey.zdasuk@tut.by>
2020-12-20 01:34:00 +0300

    Main class was added

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    public void testOne(){
+        System.out.println("Test1");
+    }
+}
